c75565abf082de8306ca15b3396292999551c3c1
nehhhhha6 <n0370525@gmail.com>
2025-05-26 01:00:36 +0530

    update

--- .next/static/chunks/app_companions_[id]_page_tsx_86588f28._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_f2b47883._.js",
-  "static/chunks/_2f668c1f._.js"
-],
-    source: "dynamic"
-});
