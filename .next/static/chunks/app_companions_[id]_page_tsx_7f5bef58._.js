65bb8b0db9b91ac327de24ea76ee9439f9c8cec5
nehhhhha6 <n0370525@gmail.com>
2025-05-26 03:39:50 +0530

    update

--- .next/static/chunks/app_companions_[id]_page_tsx_7f5bef58._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_f2b47883._.js",
-  "static/chunks/_cf57fb5b._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/app_companions_page_tsx_7f5bef58._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_bf7bf7db._.js",
-  "static/chunks/_e0755378._.js"
-],
-    source: "dynamic"
-});
